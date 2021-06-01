03c5591080794796323613c8f18e84dc2dab1001
downloadman <tri.nguyen833@gmail.com>
2021-06-02 00:31:42 +0700

    finish 1

--- masothue/main.py
+++ masothue/main.py
@@ -13,6 +13,7 @@ from selenium.common.exceptions import NoSuchElementException
 sys.stdin.reconfigure(encoding='utf-8')
 sys.stdout.reconfigure(encoding='utf-8')
 
+defautWait = 30
 
 def scroll_shim(passed_in_driver, object):  # scroll to element
     x = object.location['x']
@@ -24,51 +25,57 @@ def scroll_shim(passed_in_driver, object):  # scroll to element
 
 
 def getdataDetail(passedDriver, provineName):
+    print(passedDriver.title)
     tableInforXpath = "//table[@class='table-taxinfo']"
-    tableInfor = passedDriver.find_element_by_xpath(tableInforXpath)
-    companyNameXpath = "./thead//span"
-    companyName = tableInfor.find_element_by_xpath(
-        companyNameXpath).get_attribute('innerHTML')
-    print(companyName)
+    try:
+        tableInfor = WebDriverWait(passedDriver, defautWait).until(
+            EC.presence_of_element_located((By.XPATH, tableInforXpath)))
+        # tableInfor = passedDriver.find_element_by_xpath(tableInforXpath)
+        companyNameXpath = "./thead//span"
+        companyName = tableInfor.find_element_by_xpath(
+            companyNameXpath).get_attribute('innerHTML')
+        print(companyName)
 
-    informationsXpath = "./tbody/tr"
-    informations = tableInfor.find_elements_by_xpath(informationsXpath)
-    dataXpath1 = ".//span"
-    dataXpath2 = ".//a"
-    # get all tax info
-    for information in informations:
-        try:
-            info = information.find_element_by_xpath(dataXpath1)
-        except NoSuchElementException as ex:
+        informationsXpath = "./tbody/tr"
+        informations = tableInfor.find_elements_by_xpath(informationsXpath)
+        dataXpath1 = ".//span"
+        dataXpath2 = ".//a"
+        # get all tax info
+        for information in informations:
             try:
-                info = information.find_element_by_xpath(dataXpath2)
-            except Exception as ex:
-                info = None
-        if(info != None):
-            data = ulties.cleanhtml(info.get_attribute('innerHTML'))
-            ulties.writeData(data, provineName)
+                info = information.find_element_by_xpath(dataXpath1)
+            except NoSuchElementException as ex:
+                try:
+                    info = information.find_element_by_xpath(dataXpath2)
+                except Exception as ex:
+                    info = None
+            if(info != None):
+                data = ulties.cleanhtml(info.get_attribute('innerHTML'))
+                ulties.writeData(data, provineName)
 
-    # get all business
-    tableBusinessXpath = "//table[@class='table']"
-    tableBusiness = passedDriver.find_element_by_xpath(tableBusinessXpath)
-    businessXpath = "./tbody/tr/td[2]//a"
-    businesses = tableBusiness.find_elements_by_xpath(businessXpath)
-    ulties.writeData('___Business___')
-    for business in businesses:
-        data = ulties.cleanhtml(business.get_attribute('innerHTML'))
-        ulties.writeData(data, provineName)
+        # get all business
+        tableBusinessXpath = "//table[@class='table']"
+        tableBusiness = passedDriver.find_element_by_xpath(tableBusinessXpath)
+        businessXpath = "./tbody/tr/td[2]//a"
+        businesses = tableBusiness.find_elements_by_xpath(businessXpath)
+        ulties.writeData('___Business___', provineName)
+        for business in businesses:
+            data = ulties.cleanhtml(business.get_attribute('innerHTML'))
+            ulties.writeData(data, provineName)
+    except Exception as ex:
+        print("Error when get detail: {}".format(ex))
+
 
 
 def getDataOnePage(passedDriver, provineName):
     # get data
     print(passedDriver.title)
     xpathData = "//div[@class='tax-listing']"
-    element = WebDriverWait(passedDriver, 20).until(
+    element = WebDriverWait(passedDriver, defautWait).until(
         EC.presence_of_element_located((By.XPATH, xpathData)))
     scroll_shim(passedDriver, element)
     companies = element.find_elements_by_xpath("./div")
     for company in companies:
-        passedDriver.switch_to.window(passedDriver.window_handles[-1])
         titleDetail = company.find_element_by_xpath("./h3/a")
         scroll_shim(passedDriver, titleDetail)
         # wait before open new tab
@@ -87,8 +94,10 @@ def getDataOnePage(passedDriver, provineName):
         print("Wait {} second to close detail".format(wait))
         time.sleep(wait)
         # switch tab to last
-        passedDriver.switch_to.window(passedDriver.window_handles[-1])
         passedDriver.close()
+        passedDriver.switch_to.window(passedDriver.window_handles[-1])       
+        # testt
+        # break 
 
 
 def getDataProvine(passedDriver, provineName):
@@ -96,7 +105,7 @@ def getDataProvine(passedDriver, provineName):
     # move to next page
     xpathPageNumber = "//ul[@class='page-numbers']"
     xpathCurrentPageNumber = "//ul[@class='page-numbers']/li/span"
-    element = WebDriverWait(passedDriver, 20).until(
+    element = WebDriverWait(passedDriver, defautWait).until(
         EC.presence_of_element_located((By.XPATH, xpathPageNumber)))
     scroll_shim(passedDriver, element)
     pages = element.find_elements_by_xpath("./li")
@@ -114,9 +123,12 @@ def getDataProvine(passedDriver, provineName):
             nexPage = page.find_element_by_xpath("./a")
             nexpageNum = nexPage.get_attribute('innerHTML')
             print("____________next page____________:{}".format(nexpageNum))
+            wait = random.randint(10, 30)
+            print("Wait {} second to go next page".format(wait))
+            time.sleep(wait)
             nexPage.click()
             break
-    elementCurrentPage = WebDriverWait(passedDriver, 20).until(
+    elementCurrentPage = WebDriverWait(passedDriver, defautWait).until(
         EC.presence_of_element_located((By.XPATH, xpathCurrentPageNumber)))
     currentPageNumber = elementCurrentPage.get_attribute('innerHTML')
     if(currentPageNumber == nexpageNum):
@@ -128,13 +140,12 @@ def getDataProvine(passedDriver, provineName):
 print('start')
 # seed random number generator
 random.seed(time.perf_counter())
-# test
 
 driver = ulties.build_driver()
 driver.get('https://masothue.com/')
 xpathProvines = "//ul[@class='row']"
 
-element = WebDriverWait(driver, 10).until(
+element = WebDriverWait(driver, defautWait).until(
     EC.presence_of_element_located((By.XPATH, xpathProvines)))
 
 provinesElement = driver.find_element_by_xpath(xpathProvines)
@@ -142,7 +153,7 @@ provines = provinesElement.find_elements_by_xpath("./li/a")
 
 count = 0
 for provine in provines:
-    if(count < 1):
+    if(count < 100):
         count = count + 1
         provineTitle = provine.get_attribute('innerHTML')
         provineTitle = provineTitle[provineTitle.find("/span>")+6:].strip()
