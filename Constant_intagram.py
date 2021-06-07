82428a8e5dd1c43995154f4d2ce647cd49d56e98
nguyenminhtri <tri.nguyen833@gmail.com>
2021-06-07 17:27:26 +0700

    login intagram

---
+++ Constant_intagram.py
@@ -0,0 +1,4 @@
+defautWait = 30
+xpathMail = '//main/article/div[2]/div[1]/div/form/div/div[1]/div/label/input | //main/div/div/div[1]/div/form/div/div[1]/div/label/input'
+xpathPass = '//main/div/div/div[1]/div/form/div/div[2]/div/label/input | //main/article/div[2]/div[1]/div/form/div/div[2]/div/label/input'
+xpathButtonLogin = '//main/div/div/div[1]/div/form/div/div[3] | //main/article/div[2]/div[1]/div/form/div/div[3]'
